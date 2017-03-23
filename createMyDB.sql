d7bc26434fcea3a80c80c5590238a09ee1b15737
Jordan Brack <jbrack14@gmail.com>
2017-03-23 11:30:07 -0400

    First commit. See .sql file to begin filling database with tables.

---
+++ createMyDB.sql
@@ -0,0 +1,11 @@
+create database security_db;
+
+use security_db;
+
+create table Security_Officer (
+	SSN INT(9),
+	Last_Name VARCHAR(255),
+	First_Name VARCHAR(255),
+	Phone_Number INT(10),
+	Email VARCHAR(255),
+	Address VARCHAR(255));
