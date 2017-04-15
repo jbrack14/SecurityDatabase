b8ac3f554ce9e1acb2dc6e26aa1e789afd48f67e
Jordan Brack <jbrack14@gmail.com>
2017-04-15 13:21:22 -0400

    Added basicFunction to get User_ssn. I attempted alter $_SESSION contents to only pass the account_uuid, but there is an error with the session_start function in config.

--- security/basicFunctions.php
+++ security/basicFunctions.php
@@ -1,9 +1,9 @@
 <?php
     require("config.php");
-	
+
     function isLoggedIn()
 	{
-		if(empty($_SESSION['user']))
+		if(empty($_SESSION['Account_UUID']))
 		{
 			return false;
 		}
@@ -12,13 +12,38 @@
 			return true;
 		}
 	}
-	
+
 	function isSessionExpired()
 	{
 		//TODO: In future, we will limit the time for each session.
 		return false;
 	}
-	
+
+  function getUserSSN()
+  {
+  	global $db;
+    //Get Officer SSN
+    $query = "
+        SELECT
+            Officer_SSN
+        FROM User_Accounts
+        WHERE
+        Account_UUID = :ssn
+    ";
+
+    $query_params = array(
+        ':uuid' => $_SESSION['Account_UUID']
+    );
+    try{
+        $account_officer = $db->prepare($query);
+        $result = $account_officer->execute($query_params);
+    }
+    catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); }
+    $officer_ssn = $account_officer->fetch();
+    return $officer_ssn;
+  }
+
+
 	function doLogInCheck()
 	{
 		if(isLoggedIn())
@@ -39,44 +64,44 @@
         	die("Please login first! Redirecting to ../index.html");
 		}
 	}
-	
+
 	function isSuperUser($UserUUID)
 	{
 		global $db;
-		
+
 		$query = "
-		SELECT 1 
-		FROM Security_Officer 
-		WHERE 
-			Security_Officer.Super_SSN IN 
-			(SELECT SSN 
-			FROM Security_Officer JOIN User_Accounts ON Security_Officer.SSN = User_Accounts.Officer_SSN 
+		SELECT 1
+		FROM Security_Officer
+		WHERE
+			Security_Officer.Super_SSN IN
+			(SELECT SSN
+			FROM Security_Officer JOIN User_Accounts ON Security_Officer.SSN = User_Accounts.Officer_SSN
 			WHERE User_Accounts.Account_UUID = :user_UUID)
 		";
-	
+
 		$query_params = array(
 			':user_UUID' => $UserUUID
 		);
-		
+
 		try
 		{
 			$stmt = $db->prepare($query);
 			$qResult = $stmt->execute($query_params);
 		}
     	catch(PDOException $ex)
-		{ 
-			die("Failed to run query: " . $ex->getMessage()); 
+		{
+			die("Failed to run query: " . $ex->getMessage());
 		}
-		
+
 		$result = false;
 		if($stmt->fetch())
 		{
 		  $result = true;
 		}
-		
+
 		return $result;
 	}
-	
+
 	function isSysAdmin($UserUUID)
 	{
 		//TODO: We need admin account in the future.
